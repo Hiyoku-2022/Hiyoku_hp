426aacf7201aa327d62d04d3865beadf50c7d533
k11nakamura <k.nakamura@hiyoku.co.jp>
2025-06-19 17:07:14 +0900

    2025/06/19_私たちについてのコンポーネント追加

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { HiyokuPhilosophy } from "./HiyokuPhilosophy";
+
+export default function AboutUsPage() {
+return (
+    <main className="pt-20 px-4 max-w-4xl mx-auto">
+        <div>
+        <h1 className="text-3xl font-bold mb-6">私たちについて</h1>
+        <HiyokuPhilosophy />
+        <p className="text-gray-700">これはメインコンテンツエリアです。</p>
+        <p className="text-gray-700">これはメインコンテンツエリアです。</p>
+        <p className="text-gray-700">これはメインコンテンツエリアです。</p>
+        <p className="text-gray-700">これはメインコンテンツエリアです。</p>
+        <p className="text-gray-700">これはメインコンテンツエリアです。</p>
+        <p className="text-gray-700">これはメインコンテンツエリアです。</p>
+        </div>
+        <div>
+        <h1 className="text-xl font-bold mb-6">サブコンテンツ</h1>
+        <p className="text-gray-700">これはサブコンテンツエリアです。</p>
+        <p className="text-gray-700">これはサブコンテンツエリアです。</p>
+        <p className="text-gray-700">これはサブコンテンツエリアです。</p>
+        <p className="text-gray-700">これはサブコンテンツエリアです。</p>
+        <p className="text-gray-700">これはサブコンテンツエリアです。</p>
+        <p className="text-gray-700">これはサブコンテンツエリアです。</p>
+        </div>
+        <div>
+        <h1 className="text-xl font-bold mb-6">サブコンテンツ</h1>
+        <p className="text-gray-700">これはサブコンテンツエリアです。</p>
+        <p className="text-gray-700">これはサブコンテンツエリアです。</p>
+        <p className="text-gray-700">これはサブコンテンツエリアです。</p>
+        <p className="text-gray-700">これはサブコンテンツエリアです。</p>
+        <p className="text-gray-700">これはサブコンテンツエリアです。</p>
+        <p className="text-gray-700">これはサブコンテンツエリアです。</p>
+        </div>
+    </main>
+    )
+}
