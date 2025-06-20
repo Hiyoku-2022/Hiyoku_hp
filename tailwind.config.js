5f797e8ac24b73b096fe41741c92df5b1acaf4c9
Shimaya Nobuhiko <shimayanobuhiko@gmail.com>
2025-06-20 10:39:20 +0900

    fix: Tailwind CSSのカスタムスタイルを使用できるように修正

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
