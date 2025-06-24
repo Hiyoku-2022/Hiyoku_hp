7a33fc0f62cd41e3a384b24ee9434d96d715845b
ryu <ryuta.lacrosse.soccer@gmail.com>
2025-06-24 22:01:24 +0900

    2025/06/24_二重ネスト部分の修正、エリアごとにコンポーネント化

---
+++ src/components/top/index.tsx
@@ -0,0 +1,4 @@
+export { TopMV } from "@/components/top/top-mv";
+export { TopAboutUS } from "@/components/top/top-about-us";
+export { TopNews} from  "@/components/top/top-news";
+export { TopService} from  "@/components/top/top-service";
